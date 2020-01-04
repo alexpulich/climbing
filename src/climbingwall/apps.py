ea17e17117aad24077924e0d3757dd0af0db0337
Alexey Pulich <alexpulich@gmail.com>
2020-01-04 19:14:03 +0300

    app renaming

--- src/climbingwall/apps.py
+++ src/climbingwall/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ClimbingwallConfig(AppConfig):
     name = 'climbingwall'
+    verbose_name = 'скалодром'
--- src/social_networks/apps.py
+++ src/social_networks/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class SocialNetworksConfig(AppConfig):
     name = 'social_networks'
+    verbose_name = 'социальная сеть'
